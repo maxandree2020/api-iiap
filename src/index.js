19cf6c01f523e8102806c2f9f4df23b730bfb3fc
maxandre2020 <mcruzc@unamad.edu.pe>
2022-04-05 15:44:22 -0500

    actualizacion de cors en los headers

--- src/index.js
+++ src/index.js
@@ -8,6 +8,15 @@ app.set('port',process.env.PORT || 3000);
 //midlewares
 app.use(express.json());
 
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+ 
 //routes
 app.use(require('./routes/planta.route'));
 app.use(require('./routes/fam_plantas.route'));
